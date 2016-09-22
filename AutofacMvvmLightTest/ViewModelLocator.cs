596e6460de044cc484d37050a8291fe204292129
Eric Anderson <efcela@outlook.com>
2016-09-22 07:12:20 -0500

    Initial commit

---
+++ AutofacMvvmLightTest/ViewModelLocator.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+
+namespace AutofacMvvmLightTest
+{
+    public class ViewModelLocator
+    {
+        public static IContainer Container { get; private set; }
+
+        public ViewModelLocator()
+        {
+            var builder = new ContainerBuilder();
+            Container = builder.Build();
+        }
+    }
+}
